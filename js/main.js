0be0e3b6c7c0bcd08ce84ba9b84255e93d632f47
S-Maxwell <156356305+S-Maxwell@users.noreply.github.com>
2024-07-31 08:08:58 -0400

    Update main.js
    In this commit, the main.js file was updated with the necessary information to allow the objects to function.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 console.log("javascript file is linked");
+
+let theKeys = document.querySelectorAll('.key');
+
+function logKeyboardKeyCode(event) { 
+    let targetAudio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    let targetDiv = document.querySelector(`div[data-key="${event.keyCode}"]`);
+
+    if (!targetAudio) {
+        return;
+    }
+
+    targetAudio.currentTime = 0;
+    targetAudio.play();
+
+    targetDiv.classList.add('playing'); 
+    console.log(targetAudio, targetDiv);
+}
+
+function removeHighlight(event) {
+    if (event.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+window.addEventListener('keydown', logKeyboardKeyCode);
+theKeys.forEach(
+    key => key.addEventListener(
+        'transitionend',
+        removeHighlight
+    )
+);
